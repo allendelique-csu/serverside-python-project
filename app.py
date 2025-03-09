0fedaec8f21f4203b40824de020e9bb112080706
allen_delique_csu <152633083+allendelique-csu@users.noreply.github.com>
2025-03-09 03:36:01 +0000

    add more error handling

--- app.py
+++ app.py
@@ -5,19 +5,21 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # Fetch data from the Dog API
-    breeds_response = requests.get('https://dogapi.dog/api/v2/breeds')
-    facts_response = requests.get('https://dogapi.dog/api/v2/facts?limit=5')
-    
-    if breeds_response.status_code == 200:
+    try:
+        # Fetch data from the Dog API
+        breeds_response = requests.get('https://dogapi.dog/api/v2/breeds')
+        facts_response = requests.get('https://dogapi.dog/api/v2/facts?limit=5')
+        
+        if breeds_response.status_code != 200:
+            raise requests.RequestException("Breeds Not Found")
         breeds = breeds_response.json()
-    else:
-        breeds = []
 
-    if facts_response.status_code == 200:
+        if facts_response.status_code != 200:
+            raise requests.RequestException("Facts Not Found")
         facts = facts_response.json()
-    else:
-        facts = []
+        
+    except requests.RequestException as e:
+        return render_template('errorPage.html', error=str(e))
 
     return render_template('index.html', breeds=breeds, facts=facts)
 
@@ -28,14 +30,14 @@ def breed(id):
         breed_response = requests.get(f'https://dogapi.dog/api/v2/breeds/{id}')
 
         if breed_response.status_code != 200:
-            raise requests.RequestException("Breed not found")
+            raise requests.RequestException("Breed Not Found")
         
         breed = breed_response.json()
 
         return render_template('breed.html', breed=breed)
     
     except requests.RequestException as e:
-        return render_template('404.html', error=str(e))
+        return render_template('breedNotFound.html', error=str(e))
 
 
 # @app.errorhandler(404)
